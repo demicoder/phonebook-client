1cce00ad6cd28d832a42c6e48a0520480bb54265
Maverick <supacode@gmail.com>
2020-07-10 06:12:03 +0100

    Add spinner component

--- src/components/Contact/ContactList.js
+++ src/components/Contact/ContactList.js
@@ -4,6 +4,7 @@ import { ContactContext } from '../../context/contact/contactState';
 import ContactFilter from './ContactFilter';
 import ContactItem from './ContactItem';
 import './ContactList.scss';
+import Spinner from '../layouts/Spinner';
 
 const ContactList = () => {
   const { contacts, getContacts, loading, filteredContacts } = useContext(
@@ -17,9 +18,20 @@ const ContactList = () => {
     // eslint-disable-next-line
   }, []);
 
+  if (contacts && contacts.length === 0 && !loading) {
+    return (
+      <div className="contact__list">
+        <div className="contact__empty">
+          <h2 className="contact__empty--head">No contacts</h2>
+          <p>Your phonebook is currently empty!</p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="contact__list">
-      {contacts && !loading && contacts.length ? (
+      {contacts && !loading ? (
         <Fragment>
           <ContactFilter />
           {displayContacts.map((contact) => (
@@ -27,10 +39,7 @@ const ContactList = () => {
           ))}
         </Fragment>
       ) : (
-        <div className="contact__empty">
-          <h2 className="contact__empty--head">No contacts</h2>
-          <p>Your phonebook is currently empty!</p>
-        </div>
+        <Spinner />
       )}
     </div>
   );
